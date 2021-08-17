b591e66e7c26247e2c57cb45d3f6fcddbc8bda55
Yusuke Goto <sample@email.com>
2021-08-18 00:11:22 +0900

    partially observableを解消

--- jam/observer.py
+++ jam/observer.py
@@ -4,10 +4,12 @@ import numpy as onp
 from common import *
 
 def observe(agents, agent_idx, map_h, map_w, pcpt_h, pcpt_w):
+    pcpt_yrange = map_h * 2
+    pcpt_xrange = map_w * 2
     state = onp.zeros((1, pcpt_h, pcpt_w, EnChannel.num), dtype = onp.float32)
 
-    own_obs_y = 0.5 * map_h
-    own_obs_x = 0.5 * map_w
+    own_obs_y = 0.5 * pcpt_yrange
+    own_obs_x = 0.5 * pcpt_xrange
 
     own = agents[agent_idx]
 
@@ -25,10 +27,10 @@ def observe(agents, agent_idx, map_h, map_w, pcpt_h, pcpt_w):
         rel_x1 = wall_x1 - own.x
         obs_y0, obs_x0 = rotate(rel_y0, rel_x0, 0.5 * onp.pi - own.theta)
         obs_y1, obs_x1 = rotate(rel_y1, rel_x1, 0.5 * onp.pi - own.theta)
-        obs_py0 = int((obs_y0 + own_obs_y) / map_h * pcpt_h + 0.5)
-        obs_py1 = int((obs_y1 + own_obs_y) / map_h * pcpt_h + 0.5)
-        obs_px0 = int((obs_x0 + own_obs_x) / map_w * pcpt_w + 0.5)
-        obs_px1 = int((obs_x1 + own_obs_x) / map_w * pcpt_w + 0.5)
+        obs_py0 = int((obs_y0 + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+        obs_py1 = int((obs_y1 + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+        obs_px0 = int((obs_x0 + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
+        obs_px1 = int((obs_x1 + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
         dr.line((obs_px0, obs_py0, obs_px1, obs_py1), fill = (1,), width = 1)
     state[0, :, :, EnChannel.occupy] = onp.asarray(wall_img).astype(onp.float32)
     
@@ -36,10 +38,10 @@ def observe(agents, agent_idx, map_h, map_w, pcpt_h, pcpt_w):
         rel_y = other.y - own.y
         rel_x = other.x - own.x
         obs_y, obs_x = rotate(rel_y, rel_x, 0.5 * onp.pi - own.theta)
-        obs_py0 = int((obs_y - other.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-        obs_py1 = int((obs_y + other.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-        obs_px0 = int((obs_x - other.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
-        obs_px1 = int((obs_x + other.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
+        obs_py0 = int((obs_y - other.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+        obs_py1 = int((obs_y + other.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+        obs_px0 = int((obs_x - other.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
+        obs_px1 = int((obs_x + other.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
         if  (obs_py0 <= pcpt_h - 1) and (0 <= obs_py1) and \
             (obs_px0 <= pcpt_w - 1) and (0 <= obs_px1):
             state[0, max(obs_py0, 0) : min(obs_py1 + 1, pcpt_h), max(obs_px0, 0) : min(obs_px1 + 1, pcpt_w), EnChannel.occupy] = 1.0
@@ -49,20 +51,20 @@ def observe(agents, agent_idx, map_h, map_w, pcpt_h, pcpt_w):
     rel_y = own.tgt_y - own.y
     rel_x = own.tgt_x - own.x
     obs_y, obs_x = rotate(rel_y, rel_x, 0.5 * onp.pi - own.theta)
-    obs_py0 = int((obs_y - own.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-    obs_py1 = int((obs_y + own.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-    obs_px0 = int((obs_x - own.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
-    obs_px1 = int((obs_x + own.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
+    obs_py0 = int((obs_y - own.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+    obs_py1 = int((obs_y + own.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+    obs_px0 = int((obs_x - own.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
+    obs_px1 = int((obs_x + own.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
     if  (obs_py0 <= pcpt_h - 1) and (0 <= obs_py1) and \
         (obs_px0 <= pcpt_w - 1) and (0 <= obs_px1):
         state[0, max(obs_py0, 0) : min(obs_py1 + 1, pcpt_h), max(obs_px0, 0) : min(obs_px1 + 1, pcpt_w), EnChannel.occupy] = - 1.0
 
     obs_y = 0.0
     obs_x = 0.0
-    obs_py0 = int((obs_y - own.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-    obs_py1 = int((obs_y + own.radius_m + own_obs_y) / map_h * pcpt_h + 0.5)
-    obs_px0 = int((obs_x - own.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
-    obs_px1 = int((obs_x + own.radius_m + own_obs_x) / map_w * pcpt_w + 0.5)
+    obs_py0 = int((obs_y - own.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+    obs_py1 = int((obs_y + own.radius_m + own_obs_y) / pcpt_yrange * pcpt_h + 0.5)
+    obs_px0 = int((obs_x - own.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
+    obs_px1 = int((obs_x + own.radius_m + own_obs_x) / pcpt_xrange * pcpt_w + 0.5)
     if  (obs_py0 <= pcpt_h - 1) and (0 <= obs_py1) and \
         (obs_px0 <= pcpt_w - 1) and (0 <= obs_px1):
         state[0, max(obs_py0, 0) : min(obs_py1 + 1, pcpt_h), max(obs_px0, 0) : min(obs_px1 + 1, pcpt_w), EnChannel.vy    ] = own.v
